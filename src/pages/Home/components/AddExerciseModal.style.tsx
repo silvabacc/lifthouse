613d09dd36722e3c8203bb24cf72ed642af3acdb
silvabacc <>
2023-01-08 23:40:31 +0000

    add modal

--- src/pages/Home/components/AddExerciseModal.style.tsx
+++ src/pages/Home/components/AddExerciseModal.style.tsx
@@ -2,18 +2,27 @@ import { StyleSheet } from "react-native";
 import { spacing } from "../../../style/sizes";
 import { colors } from "../../../style/colors";
 
-const style = StyleSheet.create({
-  container: {
-    display: "flex",
-    alignItems: "center",
-  },
+const style = (keyboard: boolean) =>
+  StyleSheet.create({
+    modal_container: {
+      height: keyboard ? "60%" : "40%",
+      backgroundColor: colors.page,
+      borderRadius: spacing.small,
+      margin: spacing.large,
+    },
 
-  modal_container: {
-    backgroundColor: colors.page,
-    borderRadius: spacing.small,
-    margin: spacing.large,
-    padding: spacing.large,
-  },
-});
+    inputs_container: {
+      margin: spacing.largest,
+    },
+
+    input: {
+      marginTop: spacing.largest,
+    },
+
+    add_button: {
+      marginTop: spacing.largest,
+      color: colors.accent,
+    },
+  });
 
 export default style;
--- src/pages/Home/components/AddExerciseModal.tsx
+++ src/pages/Home/components/AddExerciseModal.tsx
@@ -1,27 +1,81 @@
-import React from "react";
-import { View } from "react-native";
-import { Portal, Modal, Text } from "react-native-paper";
+import React, { useEffect, useState } from "react";
+import { Portal, Modal, TextInput, Button } from "react-native-paper";
 import style from "./AddExerciseModal.style";
-import { textStyle } from "../../../style/House.style";
+import DropDown from "react-native-paper-dropdown";
+import { View, Keyboard } from "react-native";
+import { colors } from "../../../style/colors";
 
 interface AddExerciseModalProps {
   visible: boolean;
   setVisible: (value: boolean) => void;
 }
 
+enum ExerciseType {
+  UPPER_INTENSITY = "UPPER_INTENSITY",
+  UPPER_VOLUME = "UPPER_VOLUME",
+  LOWER_INTENSITY = "LOWER_INTENSITY",
+  LOWER_VOLUME = "LOWER_VOLUME",
+}
+
 const AddExerciseModal: React.FC<AddExerciseModalProps> = ({
   visible,
   setVisible,
 }) => {
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [keyboard, setKeyboard] = useState(false);
+  const [exerciseType, setExerciseType] = useState(
+    ExerciseType.UPPER_INTENSITY
+  );
+  const exerciseList = [
+    { label: "Upper Intensity", value: ExerciseType.UPPER_INTENSITY },
+    { label: "Upper Volume", value: ExerciseType.UPPER_VOLUME },
+    { label: "Lower Intensity", value: ExerciseType.LOWER_INTENSITY },
+    { label: "Lower Volume", value: ExerciseType.LOWER_VOLUME },
+  ];
+
+  useEffect(() => {
+    const showSubscription = Keyboard.addListener("keyboardDidShow", () => {
+      setKeyboard(true);
+    });
+    const hideSubscription = Keyboard.addListener("keyboardDidHide", () => {
+      setKeyboard(false);
+    });
+
+    return () => {
+      showSubscription.remove();
+      hideSubscription.remove();
+    };
+  }, []);
+
+  const { modal_container, inputs_container, input, add_button } =
+    style(keyboard);
+
   return (
     <Portal>
       <Modal
         visible={visible}
         onDismiss={() => setVisible(false)}
-        contentContainerStyle={style.modal_container}
+        contentContainerStyle={modal_container}
       >
-        <View style={style.container}>
-          <Text style={textStyle.h3}>Add Exercise ➕</Text>
+        <View style={[inputs_container]}>
+          <DropDown
+            activeColor={colors.accent}
+            label="Exercise Type"
+            mode="outlined"
+            visible={showDropdown}
+            showDropDown={() => setShowDropdown(!showDropdown)}
+            onDismiss={() => setShowDropdown(!showDropdown)}
+            value={exerciseType}
+            setValue={(value) => setExerciseType(value)}
+            list={exerciseList}
+          />
+          <TextInput
+            activeOutlineColor={colors.accent}
+            style={input}
+            label="Exercise Name"
+            mode="outlined"
+          />
+          <Button style={add_button}>ADD</Button>
         </View>
       </Modal>
     </Portal>
