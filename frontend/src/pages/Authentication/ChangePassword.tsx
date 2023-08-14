b296afdfabc2bc2c346c7094c04edf0b5e33aec4
silvabacc <>
2023-08-14 20:20:17 +0100

    change password

---
+++ frontend/src/pages/Authentication/ChangePassword.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ChangePassword: React.FC = () => {
+  return (
+    <div>
+      <h1>Change Password</h1>
+    </div>
+  );
+};
+
+export default ChangePassword;
