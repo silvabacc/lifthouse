4ce31ccb7dcd2acdd1fdebe299bb51accc59317f
silvabacc <47926269+silvabacc@users.noreply.github.com>
2023-09-02 21:39:02 +0100

    cleanup

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import WorkoutsCard from "./components/WorkoutsCard/WorkoutsCard";
 
 import { Col, Row, Typography } from "antd";
