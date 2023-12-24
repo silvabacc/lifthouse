a5e2cf3edb51b49814d960c8a6c0a2403eb059a3
silvabacc <47926269+silvabacc@users.noreply.github.com>
2023-12-24 01:45:14 +0000

    clean up

--- src/app/lifthouse/workouts/page.tsx
+++ src/app/lifthouse/workouts/page.tsx
@@ -1,14 +1,11 @@
 "use client";
 
 import { PageStartAnimation } from "@/app/aniamtions/pageStartAnimation";
-import { createSupabaseClient } from "@/lib/supabase/client";
 
 export default function Workouts() {
-  const supabase = createSupabaseClient();
   return (
     <PageStartAnimation>
-      <h1>Welcome to Lifthouse</h1>
-      <button onClick={() => supabase.auth.signOut()}>Sign out</button>
+      <h1>Workouts!</h1>
     </PageStartAnimation>
   );
 }
