7d3c262a8066622c7ee2e49958849d8f51ce4527
silvabacc <47926269+silvabacc@users.noreply.github.com>
2023-12-24 00:49:16 +0000

    breadcrumbs based on path

--- src/app/lifthouse/components/pageInfo.tsx
+++ src/app/lifthouse/components/pageInfo.tsx
@@ -1,3 +1,47 @@
+"use client";
+
+import { Breadcrumb } from "antd";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+
 export default function PageInfo() {
-  return <div className="bg-white">a</div>;
+  const pathName = usePathname();
+
+  const breadcrumbs = generateBreadcrumbs(pathName);
+
+  const items = breadcrumbs.map((breadcrumb) => {
+    return {
+      title: (
+        <Link href={breadcrumb.path}>
+          {capitalizeFirstLetter(breadcrumb.title)}
+        </Link>
+      ),
+    };
+  });
+
+  return (
+    <div className="bg-white p-6">
+      <Breadcrumb items={items} />
+    </div>
+  );
+}
+
+function generateBreadcrumbs(pathname: string) {
+  const segments = pathname
+    .split("/")
+    .filter((segment) => segment.trim() !== ""); // Remove empty segments
+
+  let breadcrumbs = [];
+  let currentPath = "";
+
+  for (const segment of segments) {
+    currentPath += `/${segment}`;
+    breadcrumbs.push({ title: segment, path: currentPath });
+  }
+
+  return breadcrumbs;
+}
+
+function capitalizeFirstLetter(str: string) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
 }
