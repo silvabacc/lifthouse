f12fb928fc0da766fda9a7005a9c047606f59104
silvabacc <47926269+silvabacc@users.noreply.github.com>
2023-12-24 00:29:04 +0000

    select nav item based on path

--- src/app/lifthouse/components/sideNav.tsx
+++ src/app/lifthouse/components/sideNav.tsx
@@ -4,13 +4,50 @@ import { Layout, Menu } from "antd";
 import { useAppContext } from "@/app/context";
 import Image from "next/image";
 import LifthouseLogo from "@/app/assets/ifthouse_logo_black.png";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 
 const { Sider } = Layout;
 
 export default function SiderNav() {
   const { sideNavCollapsed } = useAppContext();
   const router = useRouter();
+  const pathName = usePathname();
+
+  const items = [
+    {
+      key: "1",
+      icon: <div>🏋️</div>,
+      label: "Workouts",
+      onClick: () => router.push("/lifthouse/workouts"),
+      path: "/lifthouse/workouts",
+    },
+    {
+      key: "2",
+      icon: <div>⛰️</div>,
+      label: "Exercises",
+      onClick: () => router.push("/lifthouse/exercises"),
+      path: "/lifthouse/exercises",
+    },
+    {
+      key: "3",
+      icon: <div>🥑</div>,
+      label: "Meal tracker",
+      onClick: () => router.push("/lifthouse/meal-tracker"),
+      path: "/lifthouse/meal-tracker",
+    },
+    {
+      key: "4",
+      icon: <div>⚖️</div>,
+      onClick: () => router.push("/lifthouse/weight-ins"),
+      label: "Weigh ins",
+      path: "/lifthouse/weight-ins",
+    },
+  ];
+
+  const activeKey =
+    items.find((item) => pathName.startsWith(item.path))?.key || "1";
+
+  console.log("activeKey", activeKey);
 
   return (
     <Sider
@@ -20,36 +57,7 @@ export default function SiderNav() {
       collapsed={sideNavCollapsed}
     >
       <Image className="p-2" src={LifthouseLogo} alt="" />
-      <Menu
-        theme="light"
-        defaultSelectedKeys={["1"]}
-        items={[
-          {
-            key: "1",
-            icon: <div>🏋️</div>,
-            label: "Workouts",
-            onClick: () => router.push("/lifthouse/workouts"),
-          },
-          {
-            key: "2",
-            icon: <div>⛰️</div>,
-            label: "Exercises",
-            onClick: () => router.push("/lifthouse/exercises"),
-          },
-          {
-            key: "3",
-            icon: <div>🥑</div>,
-            label: "Meal tracker",
-            onClick: () => router.push("/lifthouse/meal-tracker"),
-          },
-          {
-            key: "4",
-            icon: <div>⚖️</div>,
-            onClick: () => router.push("/lifthouse/weight-ins"),
-            label: "Weigh ins",
-          },
-        ]}
-      />
+      <Menu theme="light" defaultSelectedKeys={[activeKey]} items={items} />
     </Sider>
   );
 }
