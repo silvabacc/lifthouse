39e1d414cf5566bea73991f0e1c72a0afea7921d
silvabacc <47926269+silvabacc@users.noreply.github.com>
2024-01-06 18:57:20 +0000

    custom search component

--- src/app/lifthouse/workouts/[workoutId]/components/ExerciseCard.tsx
+++ src/app/lifthouse/workouts/[workoutId]/components/ExerciseCard.tsx
@@ -1,11 +1,11 @@
 import { useFetch } from "@/app/hooks/useFetch";
 import { LogEntry, Workout, WorkoutTemplate } from "@/lib/supabase/db/types";
-import { Input, Select, Space } from "antd";
-import { useEffect, useState } from "react";
+import { Button, Input, Select, Space } from "antd";
+import { useEffect, useRef, useState } from "react";
 import { useExercises } from "../../hooks/useExercise";
 import { IntensityRepRange, VolumeRepRange } from "../utils";
-import styles from "../page.module.css";
 import { FadeInAnimation } from "@/app/aniamtions/fadeInAnimation";
+import { DownOutlined } from "@ant-design/icons";
 
 type ExerciseCardProps = {
   workout: Workout;
@@ -40,6 +40,7 @@ export default function ExerciseCard({ workout }: ExerciseCardProps) {
     }
   }, []);
 
+  // Templates can change the rep schemes
   const repRangeSchemeMapping = {
     [WorkoutTemplate.lower_intensity]: IntensityRepRange,
     [WorkoutTemplate.upper_intensity]: IntensityRepRange,
@@ -47,13 +48,16 @@ export default function ExerciseCard({ workout }: ExerciseCardProps) {
     [WorkoutTemplate.upper_volume]: VolumeRepRange,
   };
 
+  const formatValue = (sets: number, reps: string) => `${sets}-${reps}`;
+
+  // If the template is not found, use the both rep scheme
   const repSchemeOptions = (
     repRangeSchemeMapping[
       workout.template as keyof typeof repRangeSchemeMapping
     ] || [...VolumeRepRange, ...IntensityRepRange]
   ).map((r) => ({
     label: `${r.sets} x ${r.reps}`,
-    value: `${r.sets} x ${r.reps}`,
+    value: formatValue(r.sets, r.reps),
   }));
 
   const exerciseOptions = exercises.map((e) => ({
@@ -69,11 +73,16 @@ export default function ExerciseCard({ workout }: ExerciseCardProps) {
       {workout.exercises.map((exercise) => {
         return (
           <div key={exercise.exerciseId}>
-            <SelectElement
-              options={exerciseOptions}
-              value={exercise.exerciseId}
-            />
-            <Space className="flex flex-wrap"></Space>
+            <Space className="flex flex-wrap">
+              <SelectElement
+                defaultValue={exercise.exerciseId}
+                options={exerciseOptions}
+              />
+              <SelectElement
+                options={repSchemeOptions}
+                defaultValue={formatValue(exercise.sets, exercise.reps)}
+              />
+            </Space>
             <div>Chart</div>
           </div>
         );
@@ -82,12 +91,65 @@ export default function ExerciseCard({ workout }: ExerciseCardProps) {
   );
 }
 
-//Custom search
 type SelectProps = {
   options: { label: string; value: string | number }[];
-  value: string | number;
-  onChange?: (value: string) => void;
+  defaultValue?: string | number;
+  onChange?: (value: string | number) => void;
 };
-function SelectElement({ options, value, onChange }: SelectProps) {
-  return <></>;
+function SelectElement({ options, defaultValue, onChange }: SelectProps) {
+  const [expanded, setExpnaded] = useState(false);
+  const ref = useRef<HTMLDivElement>(null);
+
+  const findOption = (value?: string | number) =>
+    options.find((o) => o.value === value);
+
+  const [optionSelected, setOptionSelected] = useState(
+    findOption(defaultValue) ?? options[0]
+  );
+
+  const onClick = () => setExpnaded(!expanded);
+
+  const handleOutsideClick = (e: MouseEvent) => {
+    if (ref.current && !ref.current.contains(e.target as Node)) {
+      setExpnaded(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleOutsideClick);
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, []);
+
+  return (
+    <div className="relative" ref={ref}>
+      <div
+        className="flex items-center cursor-pointer justify-between border border-slate-200 p-1 rounded-lg"
+        onClick={onClick}
+      >
+        <p className="pr-2">{optionSelected.label}</p>
+        <DownOutlined />
+      </div>
+      {expanded && (
+        <div className="absolute z-10 bg-white border border-slate-200 overflow-auto h-64 w-64">
+          {options.map((o) => {
+            return (
+              <div
+                onClick={() => {
+                  setOptionSelected(o);
+                  setExpnaded(false);
+                  onChange?.(o.value);
+                }}
+                className="p-1 cursor-pointer hover:bg-slate-100"
+                key={o.value}
+              >
+                {o.label}
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
 }
