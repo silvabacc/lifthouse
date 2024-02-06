971f7d0679a43697d362e0db51c102f848733c49
silvabacc <47926269+silvabacc@users.noreply.github.com>
2024-02-06 16:15:31 +0000

    Update weightLine.tsx

--- src/app/lifthouse/weight/components/weightLine.tsx
+++ src/app/lifthouse/weight/components/weightLine.tsx
@@ -1,15 +1,13 @@
 "use client";
 
-import { Skeleton } from "antd";
 import { useWeightInContext } from "../context";
 import { Line } from "@ant-design/plots";
-import WeightLineSkeleton from "./weightLine.skeleton";
 
 export default function WeightLine() {
   const { weightData } = useWeightInContext();
 
   if (weightData.length === 0) {
-    return <WeightLineSkeleton />;
+    return null;
   }
 
   const transformData = weightData.map((weight) => {
