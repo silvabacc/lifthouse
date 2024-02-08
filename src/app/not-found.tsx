8d66149f2d5153c1176713c72e6f6baada026c1b
silvabacc <47926269+silvabacc@users.noreply.github.com>
2024-02-08 22:32:33 +0000

    not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import Thinking from "./assets/thinking.png";
+import { Button, Divider, Space } from "antd";
+import { useRouter } from "next/navigation";
+
+export default function NotFound() {
+  const router = useRouter();
+  return (
+    <div
+      className={`flex h-full bg-no-repeat bg-right bg-fixed`}
+      style={{
+        backgroundImage: `url(${Thinking.src})`,
+        backgroundSize: "cover",
+      }}
+    >
+      <div className="flex justify-center items-center w-full">
+        <div className="bg-white rounded p-8 border-black shadow-2xl">
+          <h1 className="text-5xl font-bold text-center">
+            404 - page not found 😔
+          </h1>
+          <Divider />
+          <Space className="flex justify-center" direction="vertical">
+            <p className="text-center">
+              The page you are looking for doesn&apos;t exist or has been moved.
+              Sorry about that.
+            </p>
+            <div className="flex justify-center">
+              <Button onClick={() => router.push("/lifthouse")}>Go home</Button>
+            </div>
+          </Space>
+          <Divider />
+        </div>
+      </div>
+    </div>
+  );
+}
