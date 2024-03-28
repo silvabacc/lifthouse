00097d38febd21a99c07e13ef4e0076926494541
silvabacc <47926269+silvabacc@users.noreply.github.com>
2024-03-28 01:32:20 +0000

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import AntdStyledComponentsRegistry from "./components/antd";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +23,8 @@ export default function RootLayout({
         {
           <AntdStyledComponentsRegistry>
             {children}
+            <Analytics />
+            <SpeedInsights />
           </AntdStyledComponentsRegistry>
         }
       </body>
