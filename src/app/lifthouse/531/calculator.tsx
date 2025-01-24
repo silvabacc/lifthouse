7245c1135a2f756524b5957934094174e4641428
silvabacc <47926269+silvabacc@users.noreply.github.com>
2025-01-24 16:45:57 +0000

    desktop friendly 531 program

--- src/app/lifthouse/531/calculator.tsx
+++ src/app/lifthouse/531/calculator.tsx
@@ -1,4 +1,4 @@
-import { Button, Collapse, Form, InputNumber, Table } from "antd";
+import { Alert, Button, Card, Form, InputNumber, Table } from "antd";
 import { useEffect, useRef, useState } from "react";
 
 export default function Calculator() {
@@ -48,67 +48,65 @@ export default function Calculator() {
     };
   });
 
-  const items = [
-    {
-      key: "1",
-      label: <span className="font-bold m-0 mb-2">1RM calculator</span>,
-      children: (
-        <>
-          <span>
-            Using the Brzycki formula, we can estimate your maximum load for a
-            weight training exercise. All you need to input is your best weight
-            and reps performed for the exercise
-          </span>
-          <Form>
-            {[
-              { title: "Weight", setter: setWeight },
-              { title: "Reps", setter: setReps },
-            ].map((item) => (
-              <div key={item.title} className="flex items-center mt-4">
-                <div className="w-full">
-                  <Form.Item
-                    label={
-                      <span className="font-bold mr-4 w-16">{item.title}</span>
-                    }
-                    name={item.title.toLowerCase()}
-                  >
-                    <InputNumber
-                      required
-                      onChange={(value) => {
-                        setShowTable(false);
-                        item.setter(value as number);
-                      }}
-                      className="w-full"
-                      suffix="kg"
-                    />
-                  </Form.Item>
-                </div>
+  return (
+    <div>
+      <Card>
+        <h1 className="font-bold m-0 mb-2">1RM calculator</h1>
+        <span>
+          Using the Brzycki formula, we can estimate your maximum load for a
+          weight training exercise. All you need to input is your best weight
+          and reps performed for the exercise
+        </span>
+        <Form>
+          {[
+            { title: "Weight", setter: setWeight },
+            { title: "Reps", setter: setReps },
+          ].map((item) => (
+            <div key={item.title} className="flex items-center mt-4">
+              <div className="w-full">
+                <span className="font-bold mr-4 w-16">{item.title}</span>
+                <Form.Item name={item.title.toLowerCase()}>
+                  <InputNumber
+                    required
+                    onChange={(value) => {
+                      setShowTable(false);
+                      item.setter(value as number);
+                    }}
+                    className="w-full mt-4"
+                    suffix="kg"
+                  />
+                </Form.Item>
               </div>
-            ))}
-            <div className="flex justify-center mt-4">
-              <Button
-                type="primary"
-                className="w-64"
-                onClick={onCalculcate}
-                htmlType="submit"
-              >
-                Calculate
-              </Button>
             </div>
-          </Form>
-          <div ref={tableRef}>
-            <Table
-              pagination={false}
-              className={`${showTable ? "visible" : "invisible h-0"} mt-4`}
-              columns={columns}
-              bordered={false}
-              dataSource={data}
-            />
+          ))}
+          <div className="flex justify-center mt-4">
+            <Button
+              type="primary"
+              className="w-64"
+              onClick={onCalculcate}
+              htmlType="submit"
+            >
+              Calculate
+            </Button>
           </div>
-        </>
-      ),
-    },
-  ];
-
-  return <Collapse items={items} className="my-2" />;
+        </Form>
+        <div ref={tableRef}>
+          <Table
+            pagination={false}
+            className={`${showTable ? "visible" : "invisible h-0"} mt-4`}
+            columns={columns}
+            bordered={false}
+            dataSource={data}
+          />
+        </div>
+      </Card>
+      <Alert
+        className="mt-4"
+        showIcon
+        message="References"
+        description="BRZYCKI, M. (1993) Strength testing-Predicting a one-rep max from reps-to-fatigue. JOPERD, 68, p. 88-90"
+        type="info"
+      />
+    </div>
+  );
 }
